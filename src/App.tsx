388cc7386c1e17387bf0de8505a5f3d7d92d5f1f
lucdenardi <denardiluc@gmail.com>
2025-11-17 09:43:18 -0700

    working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header/header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>
+      <div>
+        test
+      </div>
     </div>
   );
 }
