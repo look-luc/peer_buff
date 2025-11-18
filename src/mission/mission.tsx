9a9a1eb295f7fe6e3697867d8b1d48fee959170d
lucdenardi <denardiluc@gmail.com>
2025-11-18 08:58:06 -0700

    worked on background and mission page

--- src/mission/mission.tsx
+++ src/mission/mission.tsx
@@ -4,10 +4,15 @@ import Header from '../header/header'
 
 function Mission(){
     return(
-        <div>
+        <div className='wrapper'>
             <Header/>
             <div className='mission'>
-
+                <h1>
+                    Our Mission
+                </h1>
+                <p className='mission_statement'>
+                We aim to provide a safe and open space for CU Boulder students to hang out or to have someone to listen or have someone to provide peer support.  College is hard, both undergrad and grad school so we want to give a space on campus that students can step out from a stressful and potentially chaotic period.  We aim to lift the weight of school and provide any help possible while also removing any stigma against mental health.  
+                </p>
             </div>
         </div>
     )
