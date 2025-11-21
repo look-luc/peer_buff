29c438f230e733f1ad21513f2d3b6c758a815485
lucdenardi <denardiluc@gmail.com>
2025-11-21 14:50:44 -0700

    worked on page dimensions

--- src/content/people.tsx
+++ src/content/people.tsx
@@ -8,17 +8,22 @@ function People(){
         <div className='wrapper'>
             <Header/>
             <div className='contact'>
-                <p className='contact-p'>
-                    <img src={luc_head_shot} alt='Luc De Nardi head shot' className='image'/>
-                    <h3 className='name'>
-                        Luc De Nardi
-                    </h3>
-                    <div>
-                        <p className='description'>
-                            A Computational Linguistic master's student at CU Boulder.  Origanly from Newark Delaware
-                        </p>
+                <h1>
+                    Meet Our Team:
+                </h1>
+                <div className='contact-details'>
+                <img src={luc_head_shot} alt='Luc De Nardi head shot' className='image'/>
+                    <div className='contact-info'>
+                        <div className='contact-info'>
+                            <h3 className='name'>
+                                Luc De Nardi
+                            </h3>
+                            <p className='description'>
+                                A Computational Linguistic master's student at CU Boulder.  Origanly from Newark Delaware
+                            </p>
+                        </div>
                     </div>
-                </p>
+                </div>
             </div>
         </div>
     )
