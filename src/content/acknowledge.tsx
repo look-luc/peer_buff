b9ab1c3c3db99ac8d35df49d786c03e819e6a1fe
lucdenardi <denardiluc@gmail.com>
2025-11-21 09:59:40 -0700

    changed acknowledge and added mission statement docx

--- src/content/acknowledge.tsx
+++ src/content/acknowledge.tsx
@@ -14,7 +14,7 @@ function Acknowledges(){
                     We at Peer Buff acknowledge that we are located on the traditional territories and ancestral homelands of the Cheyenne, Arapaho, Ute and many other Native American nations. Their forced removal from these territories has caused devastating and lasting impacts. While we and the University of Colorado Boulder can never undo or rectify the devastation wrought on Indigenous peoples, we commit to improving and enhancing engagement with Indigenous peoples and issues locally and globally.
                 </p>
                 <p className='statement'>
-                    The logo of Peer Buff has been made possible by the talented artist Roman Norquest (he/him/they) from Delaware.
+                    The logo of Peer Buff has been made possible by the talented artist Roman Norquest from Delaware.
                 </p>
             </div>
         </div>
