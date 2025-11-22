a51d3b69d6eeaf871b28cf295a4d47544c6afb11
lucdenardi <denardiluc@gmail.com>
2025-11-22 09:17:15 -0700

    worked on people's page

--- src/content/people.tsx
+++ src/content/people.tsx
@@ -12,14 +12,14 @@ function People(){
                     Meet Our Team:
                 </h1>
                 <div className='contact-details'>
-                <img src={luc_head_shot} alt='Luc De Nardi head shot' className='image'/>
+                    <img src={luc_head_shot} alt='Luc De Nardi head shot' className='image'/>
                     <div className='contact-info'>
                         <div className='contact-info'>
                             <h3 className='name'>
                                 Luc De Nardi
                             </h3>
                             <p className='description'>
-                                A Computational Linguistic master's student at CU Boulder.  Origanly from Newark Delaware
+                                A Computational Linguistic master's student at CU Boulder.  Origanly from Newark Delaware.  I believe that having someone that has gone through their own mental health struggles to talk to can help tramendantly.  If you have any questions about our volunteering opportunities or general questions about what we do, you can email me at luc.denardi@colorado.edu
                             </p>
                         </div>
                     </div>
