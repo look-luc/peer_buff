91a575333e62fa0db776a101e4ba3b95e52a1991
lucdenardi <denardiluc@gmail.com>
2025-12-13 21:29:50 -0500

    added manodnya

--- src/content/people.tsx
+++ src/content/people.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./people.css"
 import Header from '../header/header'
 import luc_head_shot from '../assets/images/luc_head_shot.jpg'
+import manodnya_head_shot from '../assets/images/manodnya_head_shot.jpeg'
 
 function People(){
     return(
@@ -14,14 +15,26 @@ function People(){
                 <div className='contact-details'>
                     <img src={luc_head_shot} alt='Luc De Nardi head shot' className='image'/>
                     <div className='contact-info'>
-                        <div className='contact-info'>
-                            <h3 className='name'>
-                                Luc De Nardi
-                            </h3>
-                            <p className='description'>
-                                A Computational Linguistic master's student at CU Boulder.  Originally from Newark Delaware.  I believe that having someone that has gone through their own mental health struggles to talk to can help tramendantly.  If you have any questions about our volunteering opportunities or general questions about what we do, you can email me at luc.denardi@colorado.edu
-                            </p>
-                        </div>
+                        <h3 className='name'>
+                            Luc De Nardi
+                        </h3>
+                        <h4 style={{ textDecoration: 'underline' }}>Co-Founder</h4>
+                        <p className='description'>
+                            A Computational Linguistic master's student at CU Boulder.  Originally from Newark Delaware.  I believe that having someone that has gone through their own mental health struggles to talk to can help tramendantly.  If you have any questions about our volunteering opportunities or general questions about what we do, you can email me at luc.denardi@colorado.edu
+                        </p>
+                    </div>
+                    
+                </div>
+                <div className='contact-details'>
+                    <img src={manodnya_head_shot} alt='Manodnya head shot' className='image'/>
+                    <div className='contact-info'>
+                        <h3 className='name'>
+                            Manodnya K H
+                        </h3>
+                        <h4 style={{ textDecoration: 'underline' }}>Co-Founder/Website help</h4>
+                        <p className='description'>
+                            A Computational Linguistic master's student at CU Boulder. {/*TODO: add rest of description here*/}
+                        </p>
                     </div>
                 </div>
             </div>
